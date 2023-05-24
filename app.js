87491ba0cc4e5909d2f8fc6fa03b2eb392e85e56
richardmazo <richard.mazo.97@hotmail.com>
2023-05-24 13:27:10 -0500

    navegabilidad desde los botones

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
 });
